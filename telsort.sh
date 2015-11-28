55c9e546cea35ad582af305cac7be3813ae76784
Heiko Rothe <heikorothe@msn.com>
2015-11-28 13:49:56 +0100

    Added file sorting for all into one

--- telsort.sh
+++ telsort.sh
@@ -3,4 +3,27 @@
 a=true
 s=true
 d=true
-files=(null)
+files=('Controlling' 'Vorstand' 'Service')
+
+# explicitly declare associative array (implicit doesn't work)
+declare -A content
+
+# grab file contents
+for i in "${files[@]}"
+do
+	# get file contents
+	content[$i]=$(cat $i)
+done
+
+if [ "$a" = true ]
+	then
+		# sort everything together
+		# join array into single var
+		all=$(printf "\n%s" "${content[@]}")
+		all=${all:1}
+		sorted=$(echo "$all"|sort -k1 -k2)
+		echo "$sorted"
+	else
+		# sort files separately
+		echo "alles seperato"
+fi
