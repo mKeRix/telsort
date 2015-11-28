0ee044edc3e3c67e2e3058214060b8c90c32aab4
Heiko Rothe <heikorothe@msn.com>
2015-11-28 14:28:29 +0100

    Added sorting files separately

--- telsort.sh
+++ telsort.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-a=true
+a=false
 s=true
 d=true
 files=('Controlling' 'Vorstand' 'Service')
+
+# output header
+echo "Telefonlistenskript WI-14C"
+echo "Matrikelnr.: 622424"
 
 # explicitly declare associative array (implicit doesn't work)
 declare -A content
@@ -22,8 +26,39 @@ if [ "$a" = true ]
 		all=$(printf "\n%s" "${content[@]}")
 		all=${all:1}
 		sorted=$(echo "$all"|sort -k1 -k2)
-		echo "$sorted"
+		
+		if [ "$s" = true ]
+			then
+				# output to console
+				echo ""
+				echo "$sorted"
+			else
+				echo "Telefonliste in Datei gespeichert, nicht am Screen ausgegeben"
+		fi
+		
+		# ALWAYS save to a file
+		echo "$sorted" > "list_sorted"
 	else
 		# sort files separately
-		echo "alles seperato"
+		declare -A sorted_array
+		
+		# loop through content array
+		for i in "${!content[@]}"
+		do
+			# sort single file
+			sorted_array[$i]=$(echo "${content[$i]}"|sort -k1 -k2)
+			
+			if [ "$s" = true ]
+				then
+					# output to console
+					echo ""
+					echo "Datei $i"
+					echo "${sorted_array[$i]}"
+				else
+					echo "Telefonliste in Datei gespeichert, nicht am Screen ausgegeben"
+			fi
+			
+			# ALWAYS save to a file
+			echo "${sorted_array[$i]}" > "${i}_sorted"
+		done
 fi
